8638126734e2c66941e7651559279d043e95a875
o-dasher <88356162+o-dasher@users.noreply.github.com>
2023-12-25 22:50:29 -0400

    Add a simple example of i18n 'inheritance'

--- example/src/i18n/en.rs
+++ example/src/i18n/en.rs
@@ -6,9 +6,7 @@ use super::i_18_n_usage::{calculus::Calculus, greetings::Greetings};
 // Yeah, i understand this is not ideal... having to specify classes, import them, kind of painful
 // dx to be honest, still good though, but could for sure be improved! Ideally we would have some
 // kind of procedural macro working here that would have as an input the default fallback i18n,
-// automagically placing the class names for you and generating a base implementation that would
-// be "inherited" by all other i18n implementations, so this way if we add another i18n key we
-// don't have to put i18n in all places asap.
+// automagically placing the class names for you.
 impl I18NFallback for I18NUsage {
     fn fallback() -> Self {
         I18NUsage {
--- example/src/i18n/ptbr.rs
+++ example/src/i18n/ptbr.rs
@@ -1,13 +1,17 @@
 use crate::i18n::i_18_n_usage::I18NUsage;
-use rusty18n::{r, I18NDynamicResource};
+use rusty18n::{r, I18NDynamicResource, I18NFallback};
 
 use super::i_18_n_usage::{calculus::Calculus, greetings::Greetings};
 
 pub fn i18n_ptbr() -> I18NUsage {
     I18NUsage {
-        greetings: Greetings { waves: r!("Olas!") },
+        greetings: Greetings {
+            waves: r!("Iai")
+        },
         calculus: Calculus {
             answers: r!(|(a, b, c)| "{a}+{b}={c} batatudo"),
         },
+        // So, we don't have to update all the i18n locales as soon as we add a single locale.
+        ..I18NUsage::fallback()
     }
 }
