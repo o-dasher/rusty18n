3f71ac97a2444587121670aff3477dbf7cf30c82
o-dasher <88356162+o-dasher@users.noreply.github.com>
2023-12-25 22:54:14 -0400

    Add disclaimer that a macro would fit better

--- example/src/i18n/ptbr.rs
+++ example/src/i18n/ptbr.rs
@@ -10,8 +10,12 @@ pub fn i18n_ptbr() -> I18NUsage {
         },
         calculus: Calculus {
             answers: r!(|(a, b, c)| "{a}+{b}={c} batatudo"),
+            ..I18NUsage::fallback().calculus // this is kind of sad, a macro would be better to
+                                             // recurse here...
         },
         // So, we don't have to update all the i18n locales as soon as we add a single locale.
+        // ideally we would specify this once and it would recurse all the way deep the i18n tree
+        // to not cumber dx in any way.
         ..I18NUsage::fallback()
     }
 }
